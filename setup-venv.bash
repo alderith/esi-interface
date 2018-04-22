1dac5b6387f79e69adc5df3eb175a5a37b82017c
Jimmy Cumming <jimmy.cumming@arm.com>
2018-04-22 15:38:22 -0500

    added basic virtualenv setup

---
+++ setup-venv.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Setup VENV
+#You will need to install virtualenv:
+# 1. sudo apt-get install python-virtualenv
+
+mkdir virtualenv-esi
+virtualenv virtualenv-esi
+source virtualenv-esi/bin/activate
---
+++ venv.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+#source this to activate the virtualenv
+
+SCRIPTPATH="$(dirname -- "$(readlink -f -- "$0")")"
+source "$SCRIPTPATH/virtualenv-esi/bin/activate"
+
